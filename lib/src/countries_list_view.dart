33e2713407e1aa4b9f5260400a11c595f2ace205
-Daniel Ioannou <dioann28@gmail.com>
2020-08-11 12:02:41 +0300

    Implement country list view

--- lib/src/countries_list_view.dart
+++ lib/src/countries_list_view.dart
@@ -1,3 +1,5 @@
+import 'package:country_picker/res/country_codes.dart';
+import 'package:country_picker/src/utils.dart';
 import 'package:flutter/material.dart';
 
 class CountriesListView extends StatefulWidget {
@@ -10,6 +12,32 @@ class CountriesListView extends StatefulWidget {
 class _CountriesListViewState extends State<CountriesListView> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ListView(
+      children:
+          countryCodes.map<Widget>((Map country) => listRow(country)).toList(),
+    );
+  }
+
+  Widget listRow(Map country) {
+    return InkWell(
+      onTap: () {},
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 5.0),
+        child: Row(
+          children: <Widget>[
+            const SizedBox(width: 20),
+            Text(
+              Utils.countryCodeToEmoji(country['iso2_cc'] as String),
+              style: const TextStyle(fontSize: 25),
+            ),
+            const SizedBox(width: 15),
+            Text(
+              country['name'] as String,
+              style: const TextStyle(fontSize: 16),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/src/utils.dart
@@ -0,0 +1,13 @@
+class Utils {
+  static String countryCodeToEmoji(String countryCode) {
+    // 0x41 is Letter A
+    // 0x1F1E6 is Regional Indicator Symbol Letter A
+    // Example :
+    // firstLetter U => 20 + 0x1F1E6
+    // secondLetter S => 18 + 0x1F1E6
+    // See: https://en.wikipedia.org/wiki/Regional_Indicator_Symbol
+    final int firstLetter = countryCode.codeUnitAt(0) - 0x41 + 0x1F1E6;
+    final int secondLetter = countryCode.codeUnitAt(1) - 0x41 + 0x1F1E6;
+    return String.fromCharCode(firstLetter) + String.fromCharCode(secondLetter);
+  }
+}
