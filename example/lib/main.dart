9584a1180bc3d4115300050cdca4eaf1d53c9d70
Zamorite <zamorite@gmail.com>
2021-05-18 18:49:01 +0400

    📑 Update documentation - README.md

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -59,21 +59,21 @@ class HomePage extends StatelessWidget {
               onSelect: (Country country) {
                 print('Select country: ${country.displayName}');
               },
+              //Optional. Sets the border radius for the bottomsheet.
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(20.0),
+                topRight: Radius.circular(20.0),
+              ),
               //Optional. Styles the search field.
               inputDecoration: InputDecoration(
-                labelText: 'Filter',
-                hintText: 'Start typing to filter',
+                labelText: 'Search',
+                hintText: 'Start typing to search',
                 prefixIcon: const Icon(Icons.search),
                 border: OutlineInputBorder(
                   borderSide: BorderSide(
                     color: const Color(0xFF8C98A8).withOpacity(0.2),
                   ),
                 ),
-              ),
-              //Optional. Sets the border radius for the bottomsheet.
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(20.0),
-                topRight: Radius.circular(20.0),
               ),
             );
           },
