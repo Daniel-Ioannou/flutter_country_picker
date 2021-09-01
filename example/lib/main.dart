55abc90a59fa544c8dbc0302ab812f519af7a7f4
Zamorite <zamorite@gmail.com>
2021-09-01 13:42:54 +0400

    👌🏽 Revert unnecessary changes

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -13,7 +13,6 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      locale: Locale('ar'),
       supportedLocales: [
         const Locale('en'),
         const Locale('ar'),
