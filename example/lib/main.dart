9cf9013d24460cc4eb5ce26c3b9a8409bc22cce3
rsanjuan87 <rsanjuan1987@gmail.com>
2023-06-12 09:23:57 +0200

    Add Cuba to favorite countries list in country picker widget.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -116,7 +116,7 @@ class HomePage extends StatelessWidget {
                     context: context,
                     //Optional.  Can be used to exclude(remove) one ore more country from the countries list (optional).
                     exclude: <String>['KN', 'MF'],
-                    favorite: <String>['SE'],
+                    favorite: <String>['SE', 'CU'],
                     //Optional. Shows phone code before the country name.
                     showPhoneCode: true,
                     onSelect: (Country country) {
