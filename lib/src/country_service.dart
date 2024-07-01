1fb1fb60bfc57e8620caca091a8f535891be3468
Yelaman Yelmuratov <e99xxx@gmail.com>
2024-07-01 23:18:37 +0500

    @ F added phone masks

--- lib/src/country_service.dart
+++ lib/src/country_service.dart
@@ -6,20 +6,19 @@ import 'res/country_codes.dart';
 class CountryService {
   final List<Country> _countries;
 
-  CountryService()
-      : _countries =
-            countryCodes.map((country) => Country.from(json: country)).toList();
+  CountryService() : _countries = countryCodes.map((country) => Country.from(json: country)).toList();
 
   ///Return list with all countries
   List<Country> getAll() {
     return _countries;
   }
 
+  static final List<Map<String, dynamic>> getCountryCodes = countryCodes;
+
   ///Returns the first country that mach the given code.
   Country? findByCode(String? code) {
     final uppercaseCode = code?.toUpperCase();
-    return _countries
-        .firstWhereOrNull((country) => country.countryCode == uppercaseCode);
+    return _countries.firstWhereOrNull((country) => country.countryCode == uppercaseCode);
   }
 
   ///Returns the first country that mach the given name.
@@ -29,8 +28,7 @@ class CountryService {
 
   ///Returns a list with all the countries that mach the given codes list.
   List<Country> findCountriesByCode(List<String> codes) {
-    final List<String> _codes =
-        codes.map((code) => code.toUpperCase()).toList();
+    final List<String> _codes = codes.map((code) => code.toUpperCase()).toList();
     final List<Country> countries = [];
     for (final code in _codes) {
       final Country? country = findByCode(code);
