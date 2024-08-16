d6e395a212c8d9693010057a215a8b8bdb5516ae
Mohmadkasam <57990192+kasims07@users.noreply.github.com>
2024-08-16 19:16:55 +0530

    Update country_parser.dart

--- lib/src/country_parser.dart
+++ lib/src/country_parser.dart
@@ -136,8 +136,8 @@ class CountryParser {
   /// Returns a country that matches the [countryCode] (e164_cc).
   static Country _getFromPhoneCode(String phoneCode) {
     return Country.from(
-      json:  countryCodes.firstWhere(
-            (Map<String, dynamic> c) => c['e164_cc'] == phoneCode,
+      json: countryCodes.firstWhere(
+        (Map<String, dynamic> c) => c['e164_cc'] == phoneCode,
       ),
     );
   }
